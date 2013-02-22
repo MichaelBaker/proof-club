e31536d789db620150e327c3062bdc507ef4a4b8
Michael Baker <michaeltbaker@gmail.com>
2013-02-22 11:05:09 -0600

    Fixes the indentation of the body element

--- src/Markup.hs
+++ src/Markup.hs
@@ -27,23 +27,23 @@ instance ToMarkup Schedule where
         link ! rel "stylesheet" ! href "/css/fonts.css"
         link ! rel "stylesheet" ! href "/css/application.css"
 
-        body $ do
-          div ! id "content" $ do
-            Html5.section ! class_ "title" $ do
-              div $ do
-                h1 "Proof Club"
+      body $ do
+        div ! id "content" $ do
+          Html5.section ! class_ "title" $ do
+            div $ do
+              h1 "Proof Club"
 
-            Html5.section ! class_ "book" $ do
-              div $ do
-                h2 "How to Prove It"
-                a ! href "http://www.amazon.com/How-Prove-It-Structured-Approach/dp/0521675995/ref=dp_ob_title_bk" $ do
-                  img ! src "http://www.ebook3000.com/upimg/201004/21/211706562277.jpeg"
-                p "Geared to preparing students to make the transition from solving problems to proving theorems, this text teaches them the techniques needed to read and write proofs. The book begins with the basic concepts of logic and set theory, to familiarize students with the language of mathematics and how it is interpreted. These concepts are used as the basis for a step-by-step breakdown of the most important techniques used in constructing proofs. To help students construct their own proofs, this new edition contains over 200 new exercises, selected solutions, and an introduction to Proof Designer software. No background beyond standard high school mathematics is assumed."
+          Html5.section ! class_ "book" $ do
+            div $ do
+              h2 "How to Prove It"
+              a ! href "http://www.amazon.com/How-Prove-It-Structured-Approach/dp/0521675995/ref=dp_ob_title_bk" $ do
+                img ! src "http://www.ebook3000.com/upimg/201004/21/211706562277.jpeg"
+              p "Geared to preparing students to make the transition from solving problems to proving theorems, this text teaches them the techniques needed to read and write proofs. The book begins with the basic concepts of logic and set theory, to familiarize students with the language of mathematics and how it is interpreted. These concepts are used as the basis for a step-by-step breakdown of the most important techniques used in constructing proofs. To help students construct their own proofs, this new edition contains over 200 new exercises, selected solutions, and an introduction to Proof Designer software. No background beyond standard high school mathematics is assumed."
 
-            Html5.section ! class_ "schedule" $ do
-              div $ do
-                h3 "Schedule"
-                ul $ forM_ meetings $ toMarkup
+          Html5.section ! class_ "schedule" $ do
+            div $ do
+              h3 "Schedule"
+              ul $ forM_ meetings $ toMarkup
 
 instance ToMarkup Meeting where
   toMarkup (Meeting sections month day year) =
